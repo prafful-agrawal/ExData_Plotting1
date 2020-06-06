293d1c72b3c9cc020b6a4b187b92124cf26e5a5e
prafful-agrawal <prafful.agrawal.9863@gmail.com>
2020-06-06 21:50:07 +0530

    Initial commit - uploading the scripts and plots

---
+++ plot1.R
@@ -0,0 +1,44 @@
+## Load the 'lubridate' package
+library(lubridate)
+
+## Check and download the data if not yet downloaded
+if(!file.exists("./data")) {dir.create("data")}                         # Check to see if the directory exists
+if(!file.exists("./data/household_power_consumption.zip")) {
+  url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+  download.file(url, destfile = "./data/household_power_consumption.zip")
+}
+
+## Unzip the dataset and store in 'data' directory
+if(!file.exists("./data/household_power_consumption.txt")) {
+  unzip(zipfile = "./data/household_power_consumption.zip", exdir = "./data")
+}
+
+## Read the household data for the dates- 2007-02-01 and 2007-02-02
+conn <- file("./data/household_power_consumption.txt", open = "r")
+col_names <- readLines(conn, 1)                                         # Extract the column names
+lines <- c()
+while(TRUE) {
+  line <- readLines(conn, 1)
+  if(length(line) == 0) {break}
+  else if(grepl("^1\\/2\\/2007", line) || grepl("^2\\/2\\/2007", line)) {lines <- c(lines, line)}
+}
+close(conn)                                                             # Remember to close the file
+
+## Convert the data into a dataframe
+household_data <- read.table(text = lines, header = FALSE, sep = ";", na.strings = "?", stringsAsFactors = FALSE)
+colnames(household_data) <- strsplit(col_names, ";")[[1]]
+
+## Format the Date and Time values
+household_data$Date <- dmy(household_data$Date)
+tm <- paste(as.character(household_data$Date), household_data$Time)     # Add the Date values to the Time column
+household_data$Time <- strptime(tm, "%Y-%m-%d %H:%M:%S")
+
+## Create the 'plot1.png'
+png(file = "plot1.png", width = 480, height = 480, units = "px")        # Open the PNG device
+ttl <- "Global Active Power"
+x_lab <- "Global Active Power (kilowatts)"
+y_lab <- "Frequency"
+hist(household_data$Global_active_power, col = "red", main = ttl, xlab = x_lab, ylab = y_lab)
+dev.off()                                                               # Close the PNG device
+
+## END
---
+++ plot4.R
@@ -0,0 +1,69 @@
+## Load the 'lubridate' package
+library(lubridate)
+
+## Check and download the data if not yet downloaded
+if(!file.exists("./data")) {dir.create("data")}                         # Check to see if the directory exists
+if(!file.exists("./data/household_power_consumption.zip")) {
+  url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+  download.file(url, destfile = "./data/household_power_consumption.zip")
+}
+
+## Unzip the dataset and store in 'data' directory
+if(!file.exists("./data/household_power_consumption.txt")) {
+  unzip(zipfile = "./data/household_power_consumption.zip", exdir = "./data")
+}
+
+## Read the household data for the dates- 2007-02-01 and 2007-02-02
+conn <- file("./data/household_power_consumption.txt", open = "r")
+col_names <- readLines(conn, 1)                                         # Extract the column names
+lines <- c()
+while(TRUE) {
+  line = readLines(conn, 1)
+  if(length(line) == 0) {break}
+  else if(grepl("^1\\/2\\/2007", line) || grepl("^2\\/2\\/2007", line)) {lines <- c(lines, line)}
+}
+close(conn)                                                             # Remember to close the file
+
+## Convert the data into a dataframe
+household_data <- read.table(text = lines, header = FALSE, sep = ";", na.strings = "?", stringsAsFactors = FALSE)
+colnames(household_data) <- strsplit(col_names, ";")[[1]]
+
+## Format the Date and Time values
+household_data$Date <- dmy(household_data$Date)
+tm <- paste(as.character(household_data$Date), household_data$Time)     # Add the Date to the Time column
+household_data$Time <- strptime(tm, "%Y-%m-%d %H:%M:%S")
+
+## Create the 'plot4.png'
+png(file = "plot4.png", width = 480, height = 480, units = "px")        # Open the PNG device
+par(mfcol = c(2,2))                                                     # Layout the four plots
+x <- household_data$Time
+
+# First Plot
+y <- household_data$Global_active_power
+plot(x, y, xlab = "", ylab = "Global Active Power", type = "n")
+lines(x, y)
+
+# Second Plot
+y1 <- household_data$Sub_metering_1
+y2 <- household_data$Sub_metering_2
+y3 <- household_data$Sub_metering_3
+plot(x, y1, xlab = "", ylab = "Energy sub metering", type = "n")
+lines(x, y1, col = "black")
+lines(x, y2, col = "red")
+lines(x, y3, col = "blue")
+legend("topright", lty = 1, col = c("black", "red", "blue"),            # Add the legend
+       bty = "n", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+# Third Plot
+y <- household_data$Voltage
+plot(x, y, xlab = "datetime", ylab = "Voltage", type = "n")
+lines(x, y)
+
+# Fourth Plot
+y <- household_data$Global_reactive_power
+plot(x, y, xlab = "datetime", ylab = "Global_reactive_power", type = "n")
+lines(x, y)
+
+dev.off()                                                               # Close the PNG device
+
+## END
